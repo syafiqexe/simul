776f95d4e3707fa2fe037dd37045111231e0af25
syafiqexe <ahsyafiq12@gmail.com>
2020-05-07 23:29:41 +0700

    p

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p
+{
+    class PrinterWindows
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("");
+        }
+    }
+
+
+
+    class Canon : PrinterWindows
+    {
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("canon display dimension is 9.5 x 12");
+            Console.WriteLine("canon printer printing...");
+        }
+    }
+
+    class Epson : PrinterWindows
+    {
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Epson display dimension is 10 x 11");
+            Console.WriteLine("Epson printer printing...");
+        }
+    }
+
+    class LaserJet : PrinterWindows
+    {
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Laser Jet display dimension is 12 x 12");
+            Console.WriteLine("Laser Jet printer printing...");
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace p
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("\tjenis printer");
+            Console.WriteLine("1.canon");
+            Console.WriteLine("2.epson");
+            Console.WriteLine("3.laser jet");
+            int pilihprinter = Convert.ToInt32(Console.ReadLine());
+
+            if (pilihprinter == 1)
+                printer = new Canon();
+            else if (pilihprinter == 2)
+                printer = new Epson();
+            else
+                printer = new LaserJet();
+
+            printer.show();
+
+            Console.ReadKey();
+        }
+    }
+}
